6ad27bd542d33fd69d80e5885a927f1dac0fe888
yelantsevv <yelantsevv@gmail.com>
2026-09-23 10:30:45 +0200

    feat: implement slider

--- js/products.js
+++ js/products.js
@@ -144,3 +144,30 @@ export const products = {
     },
   ],
 };
+
+export const coffeeSlider = [
+  {
+    title: "S’mores Frappuccino",
+    description:
+      "This new drink takes an espresso and mixes it with brown sugar and cinnamon before being topped with oat milk.",
+    price: "$5.50",
+    img: "./img/coffee-slider-1.png",
+    alt: "S'mores Frappuccino",
+  },
+  {
+    title: "Caramel Macchiato",
+    description:
+      "Fragrant and unique classic espresso with rich caramel-peanut syrup, with cream under whipped thick foam.",
+    price: "$5.00",
+    img: "./img/coffee-slider-2.png",
+    alt: "Caramel Macchiato",
+  },
+  {
+    title: "Ice coffee",
+    description:
+      "A popular summer drink that tones and invigorates. Prepared from coffee, milk and ice.",
+    price: "$4.50",
+    img: "./img/coffee-slider-3.png",
+    alt: "Ice coffee",
+  },
+];
